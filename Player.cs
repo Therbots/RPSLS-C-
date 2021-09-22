adb0ebe2bced23f52564dd042d62f7b4caff82da
taylor herbots <therbots@live.com>
2021-09-22 15:04:00 -0500

    created player class

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Player
+    {
+        public string chosenGesture;
+        public int score;
+        public string name;
+        public string[] gestureList;
+
+        public Player(string chosenGesture, int score, string name, string gestureList)
+        {
+            this.chosenGesture = "";
+            this.score = 0;
+            this.name = "";
+            this.gestureList = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+            Console.WriteLine(gestureList);
+        }
+    }
+}
